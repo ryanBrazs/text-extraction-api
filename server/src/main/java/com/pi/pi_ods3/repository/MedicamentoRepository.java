cc1bee5dc117aaa455eb1420e05bdcf4a7cfc3b6
EliasBRodrigues <elias.rborges3@gmail.com>
2025-05-18 23:02:59 -0300

    fix: access database using MongoDB repository

--- server/src/main/java/com/pi/pi_ods3/repository/MedicamentoRepository.java
+++ server/src/main/java/com/pi/pi_ods3/repository/MedicamentoRepository.java
@@ -2,10 +2,10 @@ package com.pi.pi_ods3.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.pi.pi_ods3.models.Medicamento;
 
-public interface MedicamentoRepository extends JpaRepository<Medicamento, Long>{
+public interface MedicamentoRepository extends MongoRepository<Medicamento, String>{
     List<Medicamento> findByNomeContainingIgnoreCase(String nome);
 }
